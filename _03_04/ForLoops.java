5ad6d6c4f872464ada1a2d79d714bf1c559b39d2
Nash-BC <130668597+Nash-BC@users.noreply.github.com>
2023-04-18 14:17:56 +0000

    Course completed

--- _03_04/ForLoops.java
+++ _03_04/ForLoops.java
@@ -3,13 +3,23 @@ package _03_04;
 public class ForLoops {
 
   public static void main(String[] args) {
+    System.out.println("===================================");
     // Write a for loop that prints out the phrase "I love for loops" 5 times
-
-    // Write a for loop that prints out the numbers 1 to 10
-
-    // Write a for loop that prints out the numbers 10 to 1
+    for (int i = 0; i < 5; i++) {
+      System.out.println(i+1 + ": I love for loops");
+    }
     
+    System.out.println("===================================");
+    // Write a for loop that prints out the numbers 1 to 10
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i+1);
+    }
 
+    System.out.println("===================================");
+    // Write a for loop that prints out the numbers 10 to 1
+    for (int i = 10; i > 0; i--) {
+      System.out.println(i);
+    }
   }
 
 }
